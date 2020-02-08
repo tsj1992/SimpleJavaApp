12c26ac17cef245940098009fabcb32d2091b2f4
thilina <92thilina@gmail.com>
2020-02-08 15:36:21 +0530

    added feilds and methods

--- src/algo/Algorithm.java
+++ src/algo/Algorithm.java
@@ -2,5 +2,18 @@ package algo;
 
 public abstract class Algorithm<T>
 {
+    private T data;
+    private boolean cloneData;
 
+    public abstract void setData( T data );
+
+    public boolean isCloneData()
+    {
+        return cloneData;
+    }
+
+    public void setCloneData( boolean cloneData )
+    {
+        this.cloneData = cloneData;
+    }
 }
