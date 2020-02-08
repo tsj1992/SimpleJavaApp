7c36268026b68fe544e4b97809b12c7bd7044c0a
thilina <92thilina@gmail.com>
2020-02-08 14:51:35 +0530

    Project commit to repo

---
+++ src/SimpleApp.java
@@ -0,0 +1,19 @@
+public class SimpleApp
+{
+    private static String name = null;
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello There" );
+    }
+
+    public static String getName()
+    {
+        return name;
+    }
+
+    public static void setName( String name )
+    {
+        SimpleApp.name = name;
+    }
+}
